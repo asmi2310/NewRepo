a1d347af03927dfbc2e6f31f30555cb7e22c641a
asmi2310 <48346489+asmi2310@users.noreply.github.com>
2021-10-19 17:52:18 +0530

    Update extensions.py

--- scpat/anios/extensions.py
+++ scpat/anios/extensions.py
@@ -3,7 +3,6 @@ from flask_material import Material
 from flask_login import LoginManager, login_user
 
 import ray
-
 # from flask_login.mixins import UserMixin
 db1 = SQLAlchemy()  
 bt = Material()         #the design component
